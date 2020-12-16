4c4b01250cf7b40a00c0ffbf2317a80567a2ace4
atalay <atalaykeskin@gmail.com>
2020-12-16 23:12:42 +0300

    Add project files.

---
+++ Services/ContactAPI/Contact.API.Services/ContactService.cs
@@ -0,0 +1,18 @@
+﻿using Contact.API.Infrastructure;
+using Contact.API.Models;
+
+namespace Contact.API.Services
+{
+    public class ContactService : IContactService
+    {
+        public ContactDTO GetContactByID(int Id)
+        {
+            return new ContactDTO()
+            {
+                Id = Id,
+                FirstName="Atalay",
+                LastName="Keskin"
+            };
+        }
+    }
+}
---
+++ Services/ResAPI/Reservation.API.Services/ReservationService.cs
@@ -0,0 +1,23 @@
+﻿using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+using System;
+
+namespace Reservation.API.Services
+{
+    public class ReservationService : IReservationService
+    {
+        public ReservationDTO GetReservationByID(int BkgNumber)
+        {
+            return new ReservationDTO()
+            {
+                Id = (new Random()).Next(100),
+                BkgNumber = BkgNumber,
+                Amount = (new Random()).Next(1000),
+                BkgDate = DateTime.Now,
+                CheckinDate = DateTime.Now.Date.AddDays(30),
+                CheckoutDate = DateTime.Now.Date.AddDays(37)
+
+            };
+        }
+    }
+}
